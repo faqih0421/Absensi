cdfbba164e8076f82b76e3d39d0ef6db5d00bd48
faqih0421 <amarullohfaqih@gmail.com>
2026-09-14 16:57:00 +0700

    Fix: hapus turbopack.root yang bikin error di Vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  turbopack: {
-    root: "C:\\Users\\pc\\Documents\\folder faqih\\absensin mts",
-  },
+
 };
 
 export default nextConfig;
